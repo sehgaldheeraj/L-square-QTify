150cdc6dfeccfdcae4d38c4757afff1325bfbef7
Dheeraj <dheerajsehgal0012@gmail.com>
2023-11-09 20:29:56 +0530

    hero-qtify-takehome

---
+++ qtify/src/Components/TheHeroSection/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-anonymous-default-export */
+import "./TheHeroSection.css";
+export default () => {
+  return (
+    <div className="mainwrapper">
+      <div className="textwrapper">
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+      </div>
+      <div className="imagewrapper">
+        <img
+          src="vibrating-headphone 1.png"
+          alt="hero-img"
+          height="212px"
+          width="212px"
+        />
+      </div>
+    </div>
+  );
+};
