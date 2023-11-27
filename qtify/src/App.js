816d459d81c6b88e40cc1bfcd722ddd8aff06fa3
Dheeraj <dheerajsehgal0012@gmail.com>
2023-11-27 13:02:53 +0530

    “tabsFilter-qtify-takehome”

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -5,11 +5,16 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import TheHeroSection from "./Components/TheHeroSection";
 import Section from "./Components/Section";
+import FilterSection from "./Components/FilterSection";
+import genre from "./genres.json";
 const ENDPOINT = "https://qtify-backend-labs.crio.do/";
 
 function App() {
   const [topAlbums, setTopAlbums] = useState([]);
   const [newAlbums, setNewAlbums] = useState([]);
+  const [allSongs, setAllSongs] = useState([]);
+  // const [genre, setGenre] = useState([]);
+  const [filteredSongs, setFilteredSongs] = useState([]);
   useEffect(() => {
     axios.get(`${ENDPOINT}albums/top`).then(({ data }) => {
       setTopAlbums(data);
@@ -17,13 +22,32 @@ function App() {
     axios.get(`${ENDPOINT}albums/new`).then(({ data }) => {
       setNewAlbums(data);
     });
+    axios.get(`${ENDPOINT}songs`).then(({ data }) => {
+      setAllSongs(data);
+      setFilteredSongs(data);
+    });
   }, []);
+
   return (
     <>
       <Navbar />
       <TheHeroSection />
-      <Section title="Top Albums" data={topAlbums} />
-      <Section title="New Albums" data={newAlbums} />
+      <Section navid="ta" title="Top Albums" data={topAlbums} />
+      <Section navid="na" title="New Albums" data={newAlbums} />
+      <FilterSection
+        title="Songs"
+        data={filteredSongs}
+        filters={genre}
+        executeFilter={(genre) => {
+          if (genre === "all") {
+            setFilteredSongs(allSongs);
+          } else {
+            setFilteredSongs(
+              allSongs.filter((song) => song.genre.key === genre)
+            );
+          }
+        }}
+      />
     </>
   );
 }
---
+++ qtify/src/Components/FilterSection/index.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-anonymous-default-export */
+// import useState from "react";
+// import Section from "../Section";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+// import Box from "@mui/material/Box";
+import Carousel from "../Carousel";
+import { useState } from "react";
+import "./FilterSection.css";
+export default ({ title, data, filters, executeFilter }) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  return (
+    <div className="section">
+      <div className="section-header">
+        <h1 className="title">{title}</h1>
+      </div>
+      <Tabs
+        value={selectedTab}
+        onChange={(e, value) => {
+          executeFilter(filters[value].key);
+          setSelectedTab(value);
+        }}
+        TabIndicatorProps={{
+          style: {
+            backgroundColor: "var(--css-primary)",
+          },
+        }}
+      >
+        {filters.map((f) => (
+          <Tab className="tab" key={f.key} label={f.label} />
+        ))}
+      </Tabs>
+      <Carousel data={data} navid="filters" />
+    </div>
+  );
+};
